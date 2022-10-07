78ebc605bcd80985ee4caedf7edb4c0b41daf930
iniemiec <igorniemiec00@gmail.com>
2022-10-07 11:14:48 +0200

    unrelated rename

--- src/main/java/com/igniemie/thud/ThudApplication.java
+++ src/main/java/com/igniemie/thud/ThudApplication.java
@@ -1,13 +1,13 @@
-package com.igniemie.thud_sr;
+package com.igniemie.thud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ThudSrApplication {
+public class ThudApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ThudSrApplication.class, args);
+		SpringApplication.run(ThudApplication.class, args);
 	}
 
 }
