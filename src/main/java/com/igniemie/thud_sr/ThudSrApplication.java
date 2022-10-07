e0e7e6d3cb9ca5f8770f1def23f25b83a3bc90d9
iniemiec <igorniemiec00@gmail.com>
2022-10-07 09:15:53 +0200

    INIT

---
+++ src/main/java/com/igniemie/thud_sr/ThudSrApplication.java
@@ -0,0 +1,13 @@
+package com.igniemie.thud_sr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThudSrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThudSrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igniemie/thud_sr/ThudSrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igniemie.thud_sr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThudSrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
